016faf9b5c3edc6f2d5165e075eadbe09acfdcf9
BlindJuly <grantazach@gmail.com>
2022-05-07 13:38:53 -0500

    beenawhile

---
+++ evenPlanning.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int participants;
+    int budget;
+    int hotels;
+    int weeks;
+
+    cin >> participants >> budget >> hotels >> weeks;
+    for (int i = 0; i < hotels; i++){
+        int individualPrice;
+        cin >> individualPrice;
+        for (int j = 0; j < weeks; j++){
+            
+        }
+    }
+}
